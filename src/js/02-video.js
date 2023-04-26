23155c7cb34b144257df4c77b7c7b77b43db0411
SochaStanislaw <socha.stanislaw95@gmail.com>
2023-04-26 18:59:17 +0200

    backup of HW-8

---
+++ src/js/02-video.js
@@ -0,0 +1,39 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const savePlayerTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds), 1000;
+});
+
+const endPlayerTime = function () {
+  localStorage.removeItem('videoplayer-current-time');
+  player.unload();
+};
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+player.on('timeupdate', savePlayerTime);
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+player.on('ended', endPlayerTime);
+
+const getLocalTime = () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime === 0) return 0;
+
+  const parsedTime = JSON.parse(savedTime);
+  return Number(parsedTime);
+};
+
+const resumePlayerOnLastTime = () => {
+  const lastPlayedTime = getLocalTime();
+  player.setCurrentTime(lastPlayedTime);
+};
+
+resumePlayerOnLastTime();
